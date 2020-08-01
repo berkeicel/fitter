70e7e554cfe3f17464216263a466138c8d6895f4
icelberke <berkeicel@gmail.com>
2020-08-01 00:05:41 -0400

    Added a Screens folder which has multiple screen files. Also added starter code to every screen file.

---
+++ fitter/screens/WorkoutsDisplayScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+
+const WorkoutsDisplayScreen = props => {
+
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default WorkoutsDisplayScreen
